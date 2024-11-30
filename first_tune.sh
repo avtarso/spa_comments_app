578cf833a50a4b4f7440e7617e83862730854ce8
Avtarso <t0676352927@gmail.com>
2024-11-30 09:50:44 +0200

    Add requirements.txt

--- first_tune.sh
+++ first_tune.sh
@@ -1,10 +1,14 @@
-#chmod +x first_tune.sh
-#./first_tune.sh
+# chmod +x first_tune.sh
+# ./first_tune.sh
 
 python3 -m venv spa
 source spa/bin/activate
 
-pip install django djangorestframework
+# pip install django djangorestframework
+# pip install django-redis pillow
+
+pip install -r requirements.txt
+
 
 #django-admin startproject spaproject
 
@@ -13,4 +17,3 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver
 
-pip install django-redis pillow
