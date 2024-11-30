08703471efe60e6c30633e1f1b4e4b45080b853d
Avtarso <t0676352927@gmail.com>
2024-11-30 12:41:22 +0200

    Added temporary processing of static files

--- spaproject/comments/urls.py
+++ spaproject/comments/urls.py
@@ -4,5 +4,4 @@ from .views import CommentsListCreateView, index
 urlpatterns = [
     path('comments/', CommentsListCreateView.as_view(), name='comment-list-create'),
     path('', index, name='index'),
-
     ]
--- spaproject/spaproject/urls.py
+++ spaproject/spaproject/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from comments.views import index
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,5 +11,8 @@ urlpatterns = [
     path('', index, name='index'),
     path('api/get-captcha/', views.get_captcha, name='get_captcha'),
     path('api/verify-captcha/', views.verify_captcha, name='verify_captcha'),
-
 ]
+
+# Временная обработка статики
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
