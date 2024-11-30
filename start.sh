950c6c3fa59a66b8abfa74f4a2a9d7def1a57970
Avtarso <t0676352927@gmail.com>
2024-11-30 17:07:45 +0200

    fix font file path

--- start.sh
+++ start.sh
@@ -13,4 +13,4 @@ cd spaproject
 python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
-gunicorn spaproject.wsgi:application
+gunicorn spaproject.wsgi:application --log-level debug
