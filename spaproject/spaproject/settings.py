601ef893fed6069186c43918d08abbb7f2cfb2bc
Avtarso <t0676352927@gmail.com>
2024-12-01 14:50:27 +0200

    add new domains

--- spaproject/spaproject/settings.py
+++ spaproject/spaproject/settings.py
@@ -28,6 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     "harmonious-rebirth-production.up.railway.app",
+    "spacommentsapp-production.up.railway.app",
+    "spa.up.railway.app",
     "localhost",
     "127.0.0.1",
 ]
