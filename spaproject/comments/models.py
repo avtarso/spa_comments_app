3d11e1998cf61cc5280eda0192be6c8f1153b1b9
Avtarso <t0676352927@gmail.com>
2024-12-02 22:55:53 +0200

    Add validators on model

--- spaproject/comments/models.py
+++ spaproject/comments/models.py
@@ -1,10 +1,22 @@
+from django.core.validators import RegexValidator, URLValidator
 from django.db import models
 
 class Comment(models.Model):
 
-    user_name = models.CharField(max_length=255) 
-    e_mail = models.EmailField(max_length=255)
-    home_page = models.CharField(max_length=255, blank=True, null=True)
+    user_name = models.CharField(
+        max_length=40,
+        validators=[RegexValidator(r'^[a-zA-Z0-9]+$', 'Имя пользователя может содержать только латинские буквы и цифры!')]
+    )
+    e_mail = models.EmailField(
+        max_length=255,
+        error_messages={'invalid': 'Некорректный адрес электронной почты!'}
+    )
+    home_page = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+        validators=[URLValidator(message='Некорректный URL!')]
+    )
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
